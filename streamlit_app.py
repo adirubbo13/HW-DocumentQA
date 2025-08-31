bfc3ab3a20bbcaa7074ae943b8020249c6048701
adirubbo13 <119878526+adirubbo13@users.noreply.github.com>
2025-08-31 17:59:13 +0000

    HW1 test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def read_pdf(pdf_path): #declaring a function read the pdf files
 
 
 # Show title and description.
-st.title("Tony D's Document Question Answering for HW1")
+st.title("Tony's Document Question Answering for HW1")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
